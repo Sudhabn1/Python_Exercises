5b99ff7982f8008c2a008ce4a53d6c778e597abf
strikingraghu <42293405+strikingraghu@users.noreply.github.com>
2019-01-21 09:44:35 +0530

    Add files via upload

---
+++ Ex - 01 - Variable Types.py
@@ -0,0 +1,15 @@
+"""
+    Variables are nothing but reserved memory locations to store values.
+    This means that when you create a variable you reserve some space in memory.
+    By assigning different data types to variables, you can store integers, decimals or characters in these variables.
+"""
+
+# Assigning Values
+
+counter = 100
+miles = 95.33
+name = "John"
+
+print("Counter = ", counter)
+print("Miles =", miles)
+print("Name =", name)
