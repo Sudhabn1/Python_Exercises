c768bb4e3afd85734812cf13fb9edd9b03bb5d0c
strikingraghu <42293405+strikingraghu@users.noreply.github.com>
2019-01-22 15:40:08 +0530

    Update Ex - 03 - Decision Making.py

--- Ex - 03 - Decision Making.py
+++ Ex - 03 - Decision Making.py
@@ -18,6 +18,8 @@ if sample_variable >= 88:
 else:
     print("Variable's Value is more than our Conditional value")
 
+print("--------------------------")
+
 
 sample_string = "Hey, you are executing Python scripts!"
 another_sample_string = "python"        # p is lower case (Comparison is case-sensitive)
@@ -31,3 +33,55 @@ if yet_another_sample_string in sample_string:
     print("Yes, word 'Python' characters are present in SAMPLE_STRING's value")
 else:
     print("Hmm! Might be case-sensitive.")
+
+print("--------------------------")
+
+
+some_float_number = 4778299.882
+divide_by_number = 1883.22
+if some_float_number // divide_by_number >= 2500:       # Performs floor division
+    print("Yes, it's correct! Indeed actual floor division output =", some_float_number // divide_by_number)
+else:
+    print("No! Output of floor division is less than Comparison/Conditional value")
+
+print("--------------------------")
+
+
+# If Elif Else Statements
+
+var = 100
+if var == 200:
+    print("1 - Got a true expression value")
+    print(var)
+elif var == 150:
+    print("2 - Got a true expression value")
+    print(var)
+elif var == 100:
+    print("3 - Got a true expression value")
+    print(var)
+else:
+    print("4 - Got a false expression value")
+    print(var)
+print("Good bye!")
+
+print("--------------------------")
+
+
+sample_var_one = "Johnson"
+sample_var_two = "Cookson"
+sample_var_three = "Thomson"
+sample_var_four = "Pearson"
+validation_var_value = "Jackson"
+
+if validation_var_value == sample_var_one:
+    print("Name is matching with 'sample_var_one' variable's string value")
+elif validation_var_value == sample_var_two:
+    print("Name is matching with 'sample_var_two' variable's string value")
+elif validation_var_value == sample_var_three:
+    print("Name is matching with 'sample_var_three' variable's string value")
+elif validation_var_value == sample_var_four:
+    print("Name is matching with 'sample_var_four' variable's string value")
+else:
+    print("It's not matching with any of the above declared names! Actual name declared =", validation_var_value)
+
+print("--------------------------")
