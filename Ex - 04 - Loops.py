14b4b94ba5b080c333fc29e6c59ed76ad324b353
strikingraghu <42293405+strikingraghu@users.noreply.github.com>
2019-01-24 17:37:43 +0530

    Update Ex - 04 - Loops.py

--- Ex - 04 - Loops.py
+++ Ex - 04 - Loops.py
@@ -35,3 +35,65 @@ else:
     if another_count == 5:
         print("Ending while loop execution!")
 print("--------------------------")
+
+
+# Takes user's input
+user_input = int(input("Enter an integer value :"))
+total_value = 0
+iterable_value = 1
+
+while iterable_value <= user_input:
+    total_value = total_value + iterable_value
+    iterable_value += 1
+    print("Sum Value =", total_value)
+print("End of the execution")
+print("--------------------------")
+
+
+# For Loop
+
+sample_string = 'Bangalore'
+for each_letter in sample_string:
+    print(each_letter)
+print("Printed every letter of the word :", sample_string)
+print("--------------------------")
+
+
+# For Loop (Includes Indexing)
+
+fruits = ['Mango', 'Apple', 'Pine', 'Orange', 'Papaya']
+for each_fruit in fruits:
+    print("Fruit Name =", each_fruit, "& it's Index value =", fruits.index(each_fruit))
+print("Good Bye!")
+print("--------------------------")
+
+
+# For Loop (Is this a Prime number?)
+
+for num in range(10, 20):
+    for i in range(2, num):
+        if num % i == 0:
+            j = num / i
+            print('%d equals %d * %d' % (num, i, j))
+            break
+    else:
+            print(num, 'is a prime number')
+print("Good Bye!")
+print("--------------------------")
+
+
+# Nested Loop
+
+x = 2
+while x <= 100:
+    y = 2
+    while y <= x / y:
+        if not x % y:
+            break
+        y += 1
+    if y > x / y:
+        print(x, " is a prime number")
+    x += 1
+print("Done")
+print("--------------------------")
+
