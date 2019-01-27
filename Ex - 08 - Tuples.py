9de1a103cf0578ed4be5dc08afd88fcb66361bde
strikingraghu <42293405+strikingraghu@users.noreply.github.com>
2019-01-27 20:37:00 +0530

    Update Ex - 08 - Tuples.py

--- Ex - 08 - Tuples.py
+++ Ex - 08 - Tuples.py
@@ -26,5 +26,30 @@ print("ID - tuple_001 =", id(tuple_001))
 tuple_002 = tuple_001
 print("ID - tuple_002 =", id(tuple_001))
 print("Both tuples are having same ID's, where assignment attaches name to an object!")
+print("Assigning from one reference to another, puts two name tags to an object.")
+print(tuple_001)
+print(tuple_002)
 
+print("--------------------------")
+
+# Generic Tuple Operations
+
+tuple_007 = ('Yogesh', 'Bangalore', 'Idly', 329091, 293.3772, 'Delhi', 291, 952, 'KKR', 'RCB', 'Virat')
+print("Length =", len(tuple_007))
+print("Get Index Value =", tuple_007.index('Bangalore'))
+print("Tuple's Size =", tuple_007.__sizeof__())
+print("Tuple's Class Info =", tuple_007.__class__)
+print("Value (Virat) Contains Test =", tuple_007.__contains__('Virat'))
+print("Value (Dhoni) Contains Test =", tuple_007.__contains__('Dhoni'))
+print("Object's Attributes List =", tuple_007.__dir__())
+
+print("--------------------------")
+
+# Few Tuple Operations
+
+print("Get Value (Index Value 10) =", tuple_007.__getitem__(10))
+print("Another Way - Tuple's Length (__len__) =", tuple_007.__len__())
+tiny_tuple = ('Chennai', 'Modi')
+print("After Adding 2 Elements - ID Changed =", id(tuple_007.__add__(tiny_tuple)))
+print("However, 'tuple_007' ID =", id(tuple_007))
 print("--------------------------")
