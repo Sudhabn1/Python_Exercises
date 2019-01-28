e907db2d3d6f508ddbeac7842d77ae6311c9470f
strikingraghu <42293405+strikingraghu@users.noreply.github.com>
2019-01-28 16:06:07 +0530

    Update Ex - 11 - Functions.py

---
+++ Ex - 11 - Functions.py
@@ -0,0 +1,49 @@
+"""
+    In Python, function is a group of related statements that perform a specific task.
+    Functions help break our program into smaller and modular chunks.
+    As our program grows larger and larger, functions make it more organized and manageable.
+    Furthermore, it avoids repetition and makes code reusable.
+"""
+
+# Sample Function
+
+
+def odd_or_even(number):
+    if number % 2 == 0:
+        print(number, "= Even")
+    else:
+        print(number, "= Odd")
+
+sample_range = range(1, 21)  # Let's define a range and inject value to above mentioned function
+print()
+print("Displaying Range Elements =", sample_range)
+for each_value in sample_range:
+    odd_or_even(each_value)
+print("Iteration Done!")
+
+print("--------------------------")
+
+# Sample Function (Includes Arguments)
+
+
+def greet_person(name):
+    if type(name) is str:
+        print("Hello", name)
+        print("Have a great day ahead!")
+    else:
+        print(name, " = Name should be a string value & not any other type!")
+
+greet_person("Ramesh")  # Calling greet_person function with String value
+greet_person(32)  # Calling greet_person function with Integer value
+
+print("--------------------------")
+
+# Sample Function (User Input)
+
+
+def wishing_person(name, message="Good Morning!"):
+    print("Hello ", name)
+    print(message)
+    print("Function Completed!")
+
+wishing_person(name='Kiran')
