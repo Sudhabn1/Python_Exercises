53a5c5175382580bbb8abcdca34e6d0d86c18305
strikingraghu <42293405+strikingraghu@users.noreply.github.com>
2019-01-31 16:09:03 +0530

    Update Ex - 13 - Modules Packages.py

--- Ex - 13 - Modules Packages.py
+++ Ex - 13 - Modules Packages.py
@@ -6,12 +6,32 @@
     A module can also include runnable code.
 """
 
-
 # Import Module (name.py)
 import name
+
+# Another Mechanism (You can import module in below format)
+# from name import print_name
+# from name import *
+
+# Importing Mechanism (Import by renaming!)
+import name as name_function
+
+# Import
+import sys
 
 # Use below print command to get functions defined inside the module
 print(dir(name))  # We can see 'print_name' as an available function, which can be called from this script
 
 # Invoke Function
 name.print_name('Ramesh')
+
+# Importing Mechanism (Import by renaming!)
+# import name as name_function
+print(dir(name_function))
+print("--------------------------")
+
+# Python Module Search Path
+# import sys (It's always a good step to add import command at start of the script)
+# While importing a module, Python looks at several places.
+# Interpreter first looks for a built-in module then (if not found) into a list of directories defined in sys.path
+print(sys.path)
