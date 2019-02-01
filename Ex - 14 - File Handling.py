0a99a61f4c75c5f87a03917bfb0945db049f170f
strikingraghu <42293405+strikingraghu@users.noreply.github.com>
2019-02-01 12:04:09 +0530

    Update Ex - 14 - File Handling.py

--- Ex - 14 - File Handling.py
+++ Ex - 14 - File Handling.py
@@ -14,3 +14,20 @@ print("Class =", type(file_open))
 print("--------------------------")
 print(file_open.read())
 print("--------------------------")
+
+# Write Operations
+print("--------------------------")
+write_some_data = open("c:/python_write_operation.txt", 'w')  # New File
+write_some_data.write("Contents available in this file is been pushed via Python script!\n"
+                      "However, you can read contents without any fees\n"
+                      "By the way, are you from Bangalore city?")
+write_some_data.close()
+write_some_data = open("c:/python_write_operation.txt", 'r')
+print(write_some_data.read())
+write_some_data.close()
+print("--------------------------")
+
+# Reading Specific Lines
+write_some_data = open("c:/python_write_operation.txt", 'r')
+print(write_some_data.read(65))  # Specifically displaying few characters
+write_some_data.close()
