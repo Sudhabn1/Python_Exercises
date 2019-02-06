ea4ee86480f1c0df6b6565639796a4b9e253f8de
strikingraghu <42293405+strikingraghu@users.noreply.github.com>
2019-02-06 12:26:10 +0530

    Update Ex - 16 - Classes.py

--- Ex - 16 - Classes.py
+++ Ex - 16 - Classes.py
@@ -34,3 +34,24 @@ class Flight:
     def airline(self):
         print(self._number[:2])
         return self._number[:2]
+
+
+class Aircraft:
+
+    def __init__(self, registration, model, num_rows, num_of_seats_per_row):
+        self._registration = registration
+        self._model = model
+        self._num_rows = num_rows
+        self._num_of_seats_per_row = num_of_seats_per_row
+
+    def registration(self):
+        print(self._registration)
+        return self._registration
+
+    def model(self):
+        print(self._model)
+        return self._model
+
+    def seating_plan(self):
+        print(range(1, self._num_rows + 1), "ABCD"[:self._num_of_seats_per_row])
+        return range(1, self._num_rows + 1), "ABCD"[:self._num_of_seats_per_row]
