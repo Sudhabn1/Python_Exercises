0caef40241cdad9114d528f5272e2defbc08a6b5
strikingraghu <42293405+strikingraghu@users.noreply.github.com>
2019-02-08 17:20:44 +0530

    Update Ex - 17 - Regular Expressions.py

--- Ex - 17 - Regular Expressions.py
+++ Ex - 17 - Regular Expressions.py
@@ -11,11 +11,31 @@
 
 import re
 
+# 1st Example - Generic Code
+
 sentence = "Bengaluru is IT Capital of India and it's also Capital city for Karnataka state!"
-matchObj = re.match( r'(.*) also (.*?) .*', sentence, re.M|re.I)
+matchObj = re.match( r'(.*) also (.*?) .*', sentence, re.M | re.I)
 if matchObj:
     print("matchObj.group() : ", matchObj.group())
     print("matchObj.group(1) : ", matchObj.group(1))
     print("matchObj.group(2) : ", matchObj.group(2))
+else:
+    print("Nothing found!!")
+
+print("--------------------------")
+
+# 2nd Example Match vs Search
+
+line = "Cats are smarter than dogs"
+
+matchObj = re.match(r'dogs', line, re.M | re.I)
+if matchObj:
+    print("match --> matchObj.group() : ", matchObj.group())
+else:
+    print("No match!!")
+
+searchObj = re.search(r'dogs', line, re.M | re.I)
+if searchObj:
+    print("search --> searchObj.group() : ", searchObj.group())
 else:
     print("Nothing found!!")
