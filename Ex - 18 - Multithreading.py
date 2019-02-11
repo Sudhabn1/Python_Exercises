d93e1c6cdf41b9930a4675df0997b9f38e941b0b
strikingraghu <42293405+strikingraghu@users.noreply.github.com>
2019-02-11 11:39:42 +0530

    Update Ex - 18 - Multithreading.py

--- Ex - 18 - Multithreading.py
+++ Ex - 18 - Multithreading.py
@@ -12,6 +12,16 @@
 
         It can be pre-empted (interrupted). It can temporarily be put on hold (also known as sleeping) while other
         threads are running - this is called yielding.
+
+
+        The threading module provided with Python includes a simple-to-implement locking mechanism that allows you to
+        synchronize threads. A new lock is created by calling the Lock() method, which returns the new lock.
+        The acquire(blocking) method of the new lock object is used to force threads to run synchronously.
+        The optional blocking parameter enables you to control whether the thread waits to acquire the lock.
+        If blocking is set to 0, the thread returns immediately with a 0 value if the lock cannot be acquired
+        and with a 1 if the lock was acquired. If blocking is set to 1, the thread blocks and wait for
+        the lock to be released. The release() method of the new lock object is used to release the lock when
+        it is no longer required.
 """
 # Generic Syntax
 # thread.start_new_thread ( function, args[, kwargs] )
@@ -39,7 +49,7 @@ except:
 
 
 # Example 2
-# Define a function for the thread
+# Threading Module! Define a function for the thread
 exit_flag = 0
 
 
@@ -72,3 +82,46 @@ thread_two = MyNewThread(2, "Thread - 2", 2)
 # Start New Threads
 thread_one.start()
 thread_two.start()
+
+
+# Example 3
+# Synchronizing Threads
+
+class SimpleSyncThread(threading.Thread):
+
+    def __init__(self, threads_id, names, counters):
+        threading.Thread.__init__(self)
+        self._threads_id = threads_id
+        self._names = names
+        self._counters = counters
+
+    def run(self):
+        print("Starting Thread -", self._names)
+        print_time(self._names, 3, self._counters)
+
+
+def printing_time(threads_name, delays, counters):
+    while counters:
+        time.sleep(delays)
+        print("%s: %s" % (threads_name, time.ctime(time.time())))
+        counters -= 1
+
+thread_lock = threading.Lock()
+sync_threads = []
+
+# Creating Threads
+thread_01 = SimpleSyncThread(1, "Sync-Thread-01", 1)
+thread_02 = SimpleSyncThread(2, "Sync-Thread-02", 2)
+
+# Start Threads
+thread_01.start()
+thread_02.start()
+
+# Appending Threads
+sync_threads.append(thread_01)
+sync_threads.append(thread_02)
+
+# Wait for all threads to complete
+for thread in sync_threads:
+    thread.join()
+print("Exiting Main Thread")
