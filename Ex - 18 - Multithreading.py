a03225ebae7bbf8dbe3e76ecd97b11db576f68e6
strikingraghu <42293405+strikingraghu@users.noreply.github.com>
2019-02-12 13:48:23 +0530

    Update Ex - 18 - Multithreading.py

--- Ex - 18 - Multithreading.py
+++ Ex - 18 - Multithreading.py
@@ -27,170 +27,68 @@
 # thread.start_new_thread ( function, args[, kwargs] )
 
 import threading
-import time
-import queue
+import os
 
 # Example 1
-# Define a function for the thread
 
 
-def print_time(thread_name, delay):
-    count = 0
-    while count < 5:
-        time.sleep(delay)
-        count += 1
-        print(thread_name, time.ctime(time.time()))
+def cube_calculation(cube_number):
+    print("You have passed {} number for calculating cube root & it's value is {}!"
+          .format(cube_number, cube_number * cube_number * cube_number))
 
-# Create 2 threads as follows
-try:
-    threading._start_new_thread(print_time, ('Thread - 1', 2,))
-    threading._start_new_thread(print_time, ('Thread - 2', 4,))
-except:
-    print("Error : Unable to start thread!")
 
+def square_calculation(square_number):
+    print("You have passed {} number for calculating square root & it's value is {}!"
+          .format(square_number, square_number * square_number * square_number))
+
+if __name__ == "__main__":
+    thread_1 = threading.Thread(target=cube_calculation, args=(3,))
+    thread_2 = threading.Thread(target=square_calculation, args=(2,))
+
+    # Starting threads in this stage
+    thread_1.start()
+    thread_2.start()
+
+    # Joining both the threads for completion
+    thread_1.join()
+    thread_2.join()
+    print("Displaying thread information")
+    print(thread_1.getName(), thread_1.isAlive, thread_1.__class__)
+    print(thread_2.getName(), thread_2.isAlive, thread_2.__class__)
+    print("All steps are completed")
+else:
+    print("Issues within example # 1 code!")
+
+print("--------------------------")
 
 # Example 2
-# Threading Module! Define a function for the thread
-exit_flag = 0
 
 
-class MyNewThread(threading.Thread):
-
-    def __init__(self, thread_id, name, counter):
-        threading.Thread.__init__(self)
-        self._thread_id = thread_id
-        self._name = name
-        self._counter = counter
-
-    def run(self):
-        print("Starting =", self._name)
-        print_time(self._name, 5, self._counter)
-        print("Exiting =", self._name)
+def first_task():
+    print("first_task assigned to the thread {}".format(threading.current_thread().name))
+    print("ID of first_task running process {}".format(os.getpid()))
 
 
-def print_time(thread_name, counter, delay):
-    while counter:
-        if exit_flag:
-            thread_name.exit()
-        time.sleep(delay)
-        print("%s: %s" % (thread_name, time.ctime(time.time())))
-        counter -= 1
+def second_task():
+    print("second_task assigned to the thread {}".format(threading.current_thread().name))
+    print("ID of second_task running process {}".format(os.getpid()))
 
-# Create New Threads
-thread_one = MyNewThread(1, "Thread - 1", 1)
-thread_two = MyNewThread(2, "Thread - 2", 2)
+if __name__ == '__main__':
+    print("Main program PID value is {}".format(os.getpid()))
+    print("Main thread name is {}".format(threading.main_thread().name))
 
-# Start New Threads
-thread_one.start()
-thread_two.start()
+    # Creating 2 threads
+    thread_01 = threading.Thread(target=first_task, name='FIRST TASK')
+    thread_02 = threading.Thread(target=second_task, name='SECOND TASK')
 
+    # Starting 2 threads
+    thread_01.start()
+    thread_02.start()
 
-# Example 3
-# Synchronizing Threads
+    # Joining 2 threads
+    thread_01.join()
+    thread_02.join()
+else:
+    print("Issues within example # 2 code!")
 
-class SimpleSyncThread(threading.Thread):
-
-    def __init__(self, threads_id, names, counters):
-        threading.Thread.__init__(self)
-        self._threads_id = threads_id
-        self._names = names
-        self._counters = counters
-
-    def run(self):
-        print("Starting Thread -", self._names)
-        thread_lock.acquire()
-        print_time(self._names, 3, self._counters)
-        thread_lock.release()
-
-
-def printing_time(threads_name, delays, counters):
-    while counters:
-        time.sleep(delays)
-        print("%s: %s" % (threads_name, time.ctime(time.time())))
-        counters -= 1
-
-thread_lock = threading.Lock()
-sync_threads = []
-
-# Creating Threads
-thread_01 = SimpleSyncThread(1, "Sync-Thread-01", 1)
-thread_02 = SimpleSyncThread(2, "Sync-Thread-02", 2)
-
-# Start Threads
-thread_01.start()
-thread_02.start()
-
-# Appending Threads
-sync_threads.append(thread_01)
-sync_threads.append(thread_02)
-
-# Wait for all threads to complete
-for thread in sync_threads:
-    thread.join()
-print("Exiting Main Thread")
-
-
-# Example 4
-# Priority Threads
-
-priority_exit_flag = 0
-
-
-class PriorityQueueThread(threading.Thread):
-
-    def __init__(self, priority_thread_id, priority_thread_name, priority_thread_queue):
-        threading.Thread.__init__(self)
-        self._priority_thread = priority_thread_id
-        self._priority_thread_name = priority_thread_name
-        self._priority_thread_queue = priority_thread_queue
-
-    def run(self):
-        print("Starting Priority Thread -", self._priority_thread_name)
-        priority_queue_process(self._priority_thread_name, self._priority_thread_queue)
-        print("Exiting Priority Thread -", self._priority_thread_name)
-
-
-def priority_queue_process(priority_thread_name, priority_thread_queue):
-    while not priority_exit_flag:
-        priority_queue_lock.acquire()
-        if not priority_work_queue.empty():
-            data = priority_thread_queue.get()
-            priority_queue_lock.release()
-            print("%s processing %s" % (priority_thread_name, data))
-        else:
-            priority_queue_lock.release()
-        time.sleep(1)
-
-
-priority_thread_list = ['Priority Thread - 1', 'Priority Thread - 2', 'Priority Thread - 3']
-namesList = ["One", "Two", "Three", "Four", "Five"]
-priority_queue_lock = threading.Lock()
-priority_work_queue = queue.Queue(10)
-priority_threads = []
-priority_thread_id = 1
-
-
-# Create 2 Threads
-for priority_threads_format_name in priority_threads:
-    priority_thread = PriorityQueueThread(priority_thread_id, priority_threads_format_name, priority_work_queue)
-    priority_thread.start()
-    priority_threads.append(priority_thread)
-    priority_thread_id += 1
-
-# Fill Queue
-priority_queue_lock.acquire()
-for word in namesList:
-    priority_work_queue.put(word)
-priority_queue_lock.release()
-
-# Wait for queue to be empty
-while not priority_work_queue.empty():
-    pass
-
-# Notify threads it's time to exit
-priority_exit_flag = 1
-
-# Wait for all threads to complete
-for pthread in priority_threads:
-    pthread.join()
-print("Exiting Priority Queue Threads")
+print("--------------------------")
